c1cd2cc016b533f7ab68384a16acd51929c921c0
Appdruha <121567209+Appdruha@users.noreply.github.com>
2024-01-19 15:39:06 +0500

    подключил бд и запустил приложение

---
+++ server/environment.d.ts
@@ -0,0 +1,18 @@
+import {env} from "node:process";
+
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            env: typeof env;
+            readonly NODE_ENV: 'development' | 'production';
+            readonly PORT: number;
+            readonly DB_NAME: string;
+            readonly DB_USER: string;
+            readonly DB_PASSWORD: string;
+            readonly DB_HOST: string;
+            readonly DB_PORT: number;
+        }
+    }
+}
+
+export default global
