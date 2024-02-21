3698d46cb93b5cb51183b46e7f996a4876847982
Appdruha <121567209+Appdruha@users.noreply.github.com>
2024-02-22 00:39:31 +0500

    Загрузил material начал добавлять компоненты

--- client/src/app/layout/Layout.tsx
+++ client/src/app/layout/Layout.tsx
@@ -1,13 +1,14 @@
 import { Outlet } from 'react-router-dom'
+import { Container } from '@mui/material'
 
 export const Layout = () => {
   return (
-    <>
-      <header style={{background: 'blue', width: '100vw'}}>111</header>
+    <Container sx={{m: 'auto'}}>
+      <header style={{ background: 'blue', width: '100vw' }}>111</header>
       <main>
         <Outlet />
       </main>
-      <footer style={{background: 'red', width: '100vw'}}>222</footer>
-    </>
+      <footer style={{ background: 'red', width: '100vw' }}>222</footer>
+    </Container>
   )
 }
